8db6f456053628aa301955cc47ffccb2619ac5ec
Gaetano <gaetano.napoli@hotmail.com>
2024-02-19 23:23:28 +0100

    login e registrazione con db, fb e google, usato un .json per salvare i dati sensibili sulla mia macchina

---
+++ Login.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="ECommerce.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login Utente</title>
+    <link rel="stylesheet" type="text/css" href="Login.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Login Utente</h2>
+            <div>
+                Email: <asp:TextBox ID="txtEmail" runat="server" class="emailIn"></asp:TextBox>
+            </div>
+            <div>
+                Password: <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"  class="passwordIn"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" />
+            </div>
+            <div>
+                <a href="SignUp.aspx">Non hai un account? Registrati!</a>
+            </div>
+            <div>
+                <p>Oppure</p>
+            </div>
+            <div>
+                <asp:Button ID="btnAccediConGoogle" runat="server" Text="Accedi con Google" OnClick="btnAccediConGoogle_Click" />
+                <asp:Button ID="btnAccediConFacebook" runat="server" Text="Accedi con Facebook" OnClick="btnAccediConFacebook_Click" />
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+
+namespace ECommerce
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        private IConfiguration Configuration { get; }
+
+        public Login()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+
+            if (ValidateUser(email, password))
+            {
+                Response.Redirect("Home.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Email o password non valide. Riprova.');</script>");
+            }
+        }
+
+        private bool ValidateUser(string email, string password)
+        {
+            string connectionString = Configuration.GetConnectionString("AzureConnectionString");
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT COUNT(*) FROM ListaUtenti WHERE Email = @Email AND Password = @Password";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Password", password);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                return count > 0;
+            }
+        }
+
+        protected void btnAccediConGoogle_Click(object sender, EventArgs e)
+        {
+            string redirectUri = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/Home.aspx";
+            string googleClientId = Configuration["GoogleClientId"];
+            string googleAuthorizationUrl = "https://accounts.google.com/o/oauth2/auth?client_id=" + googleClientId + "&redirect_uri=" + redirectUri + "&response_type=code&scope=email%20profile&state=google";
+            Response.Redirect(googleAuthorizationUrl);
+        }
+
+        protected void btnAccediConFacebook_Click(object sender, EventArgs e)
+        {
+            string redirectUri = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/Home.aspx";
+            string facebookClientId = Configuration["FacebookClientId"];
+            string facebookAuthorizationUrl = "https://www.facebook.com/v12.0/dialog/oauth?client_id=" + facebookClientId + "&redirect_uri=" + redirectUri + "&response_type=code&scope=email&state=facebook";
+            Response.Redirect(facebookAuthorizationUrl);
+        }
+    }
+}
