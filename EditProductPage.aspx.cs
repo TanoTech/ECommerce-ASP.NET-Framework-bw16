daf61c049be14b68e41a1d2775e6e5f3970bfaf3
Gaetano <gaetano.napoli@hotmail.com>
2024-02-20 18:22:13 +0100

    x

---
+++ EditProductPage.aspx.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace BW16C
+{
+    public partial class EditProductPage : System.Web.UI.Page
+    {
+        private IConfiguration Configuration { get; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.QueryString["productId"] != null)
+                {
+                    int productId = Convert.ToInt32(Request.QueryString["productId"]);
+                    LoadProductDetails(productId);
+                }
+            }
+        }
+
+        public EditProductPage()
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        private void LoadProductDetails(int productId)
+        {
+            string connectionString = Configuration.GetConnectionString("AzureConnectionString");
+            string query = "SELECT Nome, Brand, Dettagli, Prezzo, Rating, Categoria, ImgUrl FROM dbo.Prodotti WHERE IdProdotto = @IdProdotto";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdProdotto", productId);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        txtProductName.Text = reader["Nome"].ToString();
+                        txtProductBrand.Text = reader["Brand"].ToString();
+                        txtProductDetails.Text = reader["Dettagli"].ToString();
+                        txtProductPrice.Text = reader["Prezzo"].ToString();
+                        txtProductRating.Text = reader["Rating"].ToString();
+                        txtProductCategory.Text = reader["Categoria"].ToString();
+                        txtImgUrl.Text = reader["ImgUrl"].ToString();
+                        lblProductId.Text = productId.ToString();
+
+                        //x immagine
+                        string imgUrl = reader["ImgUrl"].ToString();
+                        if (!string.IsNullOrEmpty(imgUrl))
+                        {
+                            imgProduct.ImageUrl = imgUrl;
+                            imgProduct.Visible = true;
+                        }
+                        else
+                        {
+                            imgProduct.Visible = false;
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            int productId = Convert.ToInt32(lblProductId.Text);
+            string nome = txtProductName.Text;
+            string brand = txtProductBrand.Text;
+            string dettagli = txtProductDetails.Text;
+            decimal prezzo = Convert.ToDecimal(txtProductPrice.Text);
+            //modifica per poter lascaire spazi vuoti e modificare prodotti
+            int rating;
+            if (!int.TryParse(txtProductRating.Text, out rating))
+            {
+                rating = 0;
+            }
+            string categoria = txtProductCategory.Text;
+            string imgUrl = txtImgUrl.Text;
+
+            string connectionString = Configuration.GetConnectionString("AzureConnectionString");
+            string query = "UPDATE dbo.Prodotti SET Nome = @Nome, Brand = @Brand, Dettagli = @Dettagli, Prezzo = @Prezzo, Rating = @Rating, Categoria = @Categoria, ImgUrl = @ImgUrl WHERE IdProdotto = @IdProdotto";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdProdotto", productId);
+                    command.Parameters.AddWithValue("@Nome", nome);
+                    command.Parameters.AddWithValue("@Brand", brand);
+                    command.Parameters.AddWithValue("@Dettagli", dettagli);
+                    command.Parameters.AddWithValue("@Prezzo", prezzo);
+                    command.Parameters.AddWithValue("@Rating", rating);
+                    command.Parameters.AddWithValue("@Categoria", categoria);
+                    command.Parameters.AddWithValue("@ImgUrl", imgUrl);
+                    connection.Open();
+                    int rowsAffected = command.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        Response.Redirect("Admin.aspx");
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProductDetails.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Templates/Master.Master" AutoEventWireup="true" CodeBehind="ProductDetails.aspx.cs" Inherits="BW16C.ProductDetails" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <title>Dettagli Prodotto</title>
+<link href="Styles/ProductDetails.css" rel="stylesheet" />
+    <script type="text/javascript">
+        function mostraMessaggioConferma(messaggio) {
+            var modal = document.getElementById('modalMessaggio');
+            var lblMessaggioConfermaModal = document.getElementById('<%= lblMessaggioConfermaModal.ClientID %>');
+
+            lblMessaggioConfermaModal.innerHTML = messaggio;
+            modal.style.display = 'block';
+
+            setTimeout(function () {
+                modal.style.display = 'none';
+            }, 3000);
+        }
+    </script>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+   <form id="form1" runat="server">
+       <div>
+           <div id="LeftContainerDetails">
+               <div id="imgContainerDetails">
+            <asp:Image ID="imgProductDetails" runat="server" CssClass="imgProductDetails"/>
+        </div>
+        <div id="textContainerDetails">
+            <div>
+                <asp:Label ID="lblProductNameDetails" runat="server" Text="Nome Prodotto:" CssClass="productNameDetails"></asp:Label>
+                <asp:Label ID="lblBrandDetails" runat="server" Text="Marca:" CssClass="brandDetails"></asp:Label>
+                <asp:Label ID="lblRatingDetails" runat="server" Text="Rating:" CssClass="ratingDetails"></asp:Label>
+                <asp:Label ID="lblPriceDetails" runat="server" Text="Prezzo:" CssClass="priceDetails"></asp:Label>
+            </div>            
+            <div>
+                <asp:Label ID="lblCategoryDetails" runat="server" Text="Categoria:" CssClass="categoryDetails"></asp:Label>
+                <asp:Label ID="lblDetailsDetails" runat="server" Text="Dettagli:" CssClass="detailsDetails"></asp:Label>
+            </div>
+        </div>
+    </div>
+    <div id="RightContainerDetails">
+        <h3>COMPRA</h3>
+        <label for="ddlQuantita">Quantità:</label>
+        <asp:DropDownList ID="ddlQuantitàDetails" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlQuantita_SelectedIndexChanged">
+            <asp:ListItem Text="1" Value="1" Selected="True"></asp:ListItem>
+            <asp:ListItem Text="2" Value="2"></asp:ListItem>
+            <asp:ListItem Text="3" Value="3"></asp:ListItem>
+            <asp:ListItem Text="4" Value="4"></asp:ListItem>
+            <asp:ListItem Text="5" Value="5"></asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <label for="lblPrezzoTotale">Prezzo Totale:</label>
+        <asp:Label ID="lblPrezzoTotaleDetails" runat="server" Text="" CssClass="prezzoTotaleDetails"></asp:Label>
+        <br />
+        <asp:Button ID="btnAggiungiAlCarrelloDetails" runat="server" Text="Aggiungi al Carrello" CssClass="btnAggiungiAlCarrelloDetails" OnClick="btnAggiungiAlCarrelloDetails_Click"/>
+        <br />
+        <div id="modalMessaggio" class="modalMessaggio">
+            <div class="modalMessaggio-content">
+                <asp:Label ID="lblMessaggioConfermaModal" runat="server" Text="" CssClass="messaggioConfermaModal"></asp:Label>
+            </div>
+        </div>
+     </div>
+</div>
+</form>
+    
+</asp:Content>
