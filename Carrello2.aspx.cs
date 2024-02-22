5ec966d9cf19fc238783015440b11d1738b313b1
Gaetano <gaetano.napoli@hotmail.com>
2024-02-22 11:12:09 +0100

    carrello cookie funzionaaa

--- Carrello2.aspx.cs
+++ Carrello2.aspx.cs
@@ -1,15 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic; 
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
-namespace BW16C 
+namespace BW16C
 {
     public partial class Carrello2 : System.Web.UI.Page
     {
-
         private IConfiguration Configuration { get; }
 
         public Carrello2()
@@ -19,25 +20,32 @@ namespace BW16C
                 .AddJsonFile("appsettings.json")
                 .Build();
         }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 if (Request.Cookies["Carrello"] != null)
                 {
-                    string carrelloCookieValue = Server.HtmlEncode(Request.Cookies["Carrello"].Value);
-                    string[] prodottiInCarrello = carrelloCookieValue.Split('&');
+                    string carrelloCookieValue = Request.Cookies["Carrello"].Value;
+
+                    // Utilizza la regex per estrarre i prodotti dal cookie "Carrello"
+                    MatchCollection matches = Regex.Matches(carrelloCookieValue, @"(?<idProdotto>\d+)=(?<quantita>\d+)");
+
+                    // Costruisci una lista di idProdotto dal cookie
+                    List<int> idProdotti = new List<int>();
+                    foreach (Match match in matches)
+                    {
+                        int idProdotto = int.Parse(match.Groups["idProdotto"].Value);
+                        idProdotti.Add(idProdotto);
+                    }
 
                     StringBuilder queryBuilder = new StringBuilder();
                     queryBuilder.Append("SELECT * FROM Prodotti WHERE IdProdotto IN (");
-
-                    foreach (string prodotto in prodottiInCarrello)
+                    foreach (int idProdotto in idProdotti)
                     {
-                        string[] idQuantita = prodotto.Split('=');
-                        int idProdotto = Convert.ToInt32(idQuantita[0]);
                         queryBuilder.Append(idProdotto + ",");
                     }
-
                     queryBuilder.Remove(queryBuilder.Length - 1, 1);
                     queryBuilder.Append(")");
 
